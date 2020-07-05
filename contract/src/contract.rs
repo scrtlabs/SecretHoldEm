8d378e36677ff384a931a7148964f4dbee01142d
Assaf Morami <assaf.morami@gmail.com>
2020-07-05 22:33:00 +0300

    implement shuffle and GetMyCard

--- contract/src/contract.rs
+++ contract/src/contract.rs
@@ -1,7 +1,10 @@
+use bincode;
 use cosmwasm_std::{
-    generic_err, Api, Env, Extern, HandleResponse, HandleResult, InitResponse, InitResult,
+    generic_err, Api, Binary, Env, Extern, HandleResponse, HandleResult, InitResponse, InitResult,
     MigrateResponse, Querier, QueryResponse, QueryResult, StdResult, Storage,
 };
+use rand::{seq::SliceRandom, SeedableRng};
+use rand_chacha::ChaChaRng;
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 use sha2::{Digest, Sha256};
@@ -13,20 +16,16 @@ use sha2::{Digest, Sha256};
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 #[serde(rename_all = "snake_case")]
 pub enum InitMsg {
-    CreateTable { seed: u64 },
+    CreateTable {},
 }
 
 pub fn init<S: Storage, A: Api, Q: Querier>(
-    deps: &mut Extern<S, A, Q>,
-    env: Env,
+    _deps: &mut Extern<S, A, Q>,
+    _env: Env,
     msg: InitMsg,
 ) -> InitResult {
     match msg {
-        InitMsg::CreateTable { seed } => {
-            deps.storage.set(b"creator", &env.message.sender.as_slice());
-            deps.storage.set(b"creator_seed", &seed.to_be_bytes());
-            Ok(InitResponse::default())
-        }
+        InitMsg::CreateTable {} => Ok(InitResponse::default()),
     }
 }
 
@@ -37,7 +36,7 @@ pub fn init<S: Storage, A: Api, Q: Querier>(
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 #[serde(rename_all = "snake_case")]
 pub enum HandleMsg {
-    Join { seed: u64 },
+    Join { secret: u64 },
 }
 
 pub fn handle<S: Storage, A: Api, Q: Querier>(
@@ -46,7 +45,7 @@ pub fn handle<S: Storage, A: Api, Q: Querier>(
     msg: HandleMsg,
 ) -> HandleResult {
     match msg {
-        HandleMsg::Join { seed } => {
+        HandleMsg::Join { secret } => {
             let player_a = deps.storage.get(b"player_a");
             let player_b = deps.storage.get(b"player_b");
 
@@ -54,32 +53,35 @@ pub fn handle<S: Storage, A: Api, Q: Querier>(
                 return Err(generic_err("Table is full."));
             }
 
-            let mut player_name = b"player_b";
+            let player_name = env.message.sender.as_slice();
+            let player_secret = &secret.to_be_bytes();
+
             if player_a.is_none() {
-                player_name = b"player_a";
+                // player a
+                deps.storage.set(b"player_a", player_name);
+                deps.storage.set(b"player_a_secret", player_secret);
+                return Ok(HandleResponse::default());
             }
 
-            let mut cards_seed = deps
-                .storage
-                .get(b"creator_seed")
-                .expect("No seed from creator.");
+            // player b - can now shuffle the deck
 
-            cards_seed.extend(&seed.to_be_bytes());
+            deps.storage.set(b"player_b", player_name);
+            deps.storage.set(b"player_b_secret", player_secret);
 
-            let x: &[u8] = Sha256::digest(&cards_seed).as_slice();
+            let player_a_secret = deps.storage.get(b"player_a_secret").unwrap();
 
-            /*
-                TODO: figure out deck shuffle logic
-            */
+            let mut combined_secret = player_a_secret.clone();
+            combined_secret.extend(player_secret);
+            let shuffle_seed: [u8; 32] = Sha256::digest(&combined_secret).into();
 
-            deps.storage
-                .set(player_name, &env.message.sender.as_slice());
+            let mut rng = ChaChaRng::from_seed(shuffle_seed);
+            let mut deck = get_new_deck();
+            deck.shuffle(&mut rng);
 
-            Ok(HandleResponse {
-                data: None, /* Hand... */
-                log: vec![],
-                messages: vec![],
-            })
+            let deck_bytes = bincode::serialize(&deck).unwrap();
+            deps.storage.set(b"deck", &deck_bytes);
+
+            return Ok(HandleResponse::default());
         }
     }
 }
@@ -92,16 +94,60 @@ pub fn handle<S: Storage, A: Api, Q: Querier>(
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 #[serde(rename_all = "snake_case")]
 pub enum QueryMsg {
+    GetMyHand { secret: u64 },
     GetAllPublicData {},
 }
 
-pub fn query<S: Storage, A: Api, Q: Querier>(
-    _deps: &Extern<S, A, Q>,
-    _msg: QueryMsg,
-) -> QueryResult {
-    match _msg {
+pub fn query<S: Storage, A: Api, Q: Querier>(deps: &Extern<S, A, Q>, msg: QueryMsg) -> QueryResult {
+    match msg {
         QueryMsg::GetAllPublicData {} => {
             return Ok(QueryResponse::default());
+        }
+        QueryMsg::GetMyHand { secret } => {
+            let secret_bytes = secret.to_be_bytes().to_vec();
+
+            let player_a_secret = match deps.storage.get(b"player_a_secret") {
+                None => {
+                    return Err(generic_err(
+                        "You are not a player, but there are still two seats left.",
+                    ))
+                }
+                Some(x) => x,
+            };
+            let player_b_secret = match deps.storage.get(b"player_b_secret") {
+                None => {
+                    return Err(generic_err(
+                        "You are not a player, but there is still one seat left.",
+                    ))
+                }
+                Some(x) => x,
+            };
+
+            let first_card_index;
+            let second_card_index;
+            if secret_bytes == player_a_secret {
+                first_card_index = 0;
+                second_card_index = 2;
+            } else if secret_bytes == player_b_secret {
+                first_card_index = 1;
+                second_card_index = 3;
+            } else {
+                return Err(generic_err("You are not a player, go away!"));
+            }
+
+            let deck_bytes = deps.storage.get(b"deck").unwrap();
+            let deck: Vec<Card> = bincode::deserialize(&deck_bytes)
+                .map_err(|e| generic_err(format!("Could not deserialze deck: {:?}", e)))?;
+
+            let first_card = (&deck)[first_card_index];
+            let second_card = (&deck)[second_card_index];
+
+            return Ok(Binary(vec![
+                first_card.number as u8,
+                first_card.shape as u8,
+                second_card.number as u8,
+                second_card.shape as u8,
+            ]));
         }
     }
 }
@@ -121,4 +167,249 @@ pub fn migrate<S: Storage, A: Api, Q: Querier>(
     _msg: MigrateMsg,
 ) -> StdResult<MigrateResponse> {
     Ok(MigrateResponse::default())
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema, Copy)]
+struct Card {
+    pub number: Number,
+    pub shape: Shape,
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema, Copy)]
+#[repr(u8)]
+enum Number {
+    Ace = 1,
+    Two,
+    Three,
+    Four,
+    Five,
+    Six,
+    Seven,
+    Eight,
+    Nine,
+    Ten,
+    Jack,
+    Queen,
+    King,
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema, Copy)]
+#[repr(u8)]
+enum Shape {
+    Spades = 1,
+    Hearts,
+    Clubs,
+    Diamonds,
+}
+fn get_new_deck() -> Vec<Card> {
+    return vec![
+        Card {
+            number: Number::Ace,
+            shape: Shape::Clubs,
+        },
+        Card {
+            number: Number::Two,
+            shape: Shape::Clubs,
+        },
+        Card {
+            number: Number::Three,
+            shape: Shape::Clubs,
+        },
+        Card {
+            number: Number::Four,
+            shape: Shape::Clubs,
+        },
+        Card {
+            number: Number::Five,
+            shape: Shape::Clubs,
+        },
+        Card {
+            number: Number::Six,
+            shape: Shape::Clubs,
+        },
+        Card {
+            number: Number::Seven,
+            shape: Shape::Clubs,
+        },
+        Card {
+            number: Number::Eight,
+            shape: Shape::Clubs,
+        },
+        Card {
+            number: Number::Nine,
+            shape: Shape::Clubs,
+        },
+        Card {
+            number: Number::Ten,
+            shape: Shape::Clubs,
+        },
+        Card {
+            number: Number::Jack,
+            shape: Shape::Clubs,
+        },
+        Card {
+            number: Number::Queen,
+            shape: Shape::Clubs,
+        },
+        Card {
+            number: Number::King,
+            shape: Shape::Clubs,
+        },
+        Card {
+            number: Number::Ace,
+            shape: Shape::Hearts,
+        },
+        Card {
+            number: Number::Two,
+            shape: Shape::Hearts,
+        },
+        Card {
+            number: Number::Three,
+            shape: Shape::Hearts,
+        },
+        Card {
+            number: Number::Four,
+            shape: Shape::Hearts,
+        },
+        Card {
+            number: Number::Five,
+            shape: Shape::Hearts,
+        },
+        Card {
+            number: Number::Six,
+            shape: Shape::Hearts,
+        },
+        Card {
+            number: Number::Seven,
+            shape: Shape::Hearts,
+        },
+        Card {
+            number: Number::Eight,
+            shape: Shape::Hearts,
+        },
+        Card {
+            number: Number::Nine,
+            shape: Shape::Hearts,
+        },
+        Card {
+            number: Number::Ten,
+            shape: Shape::Hearts,
+        },
+        Card {
+            number: Number::Jack,
+            shape: Shape::Hearts,
+        },
+        Card {
+            number: Number::Queen,
+            shape: Shape::Hearts,
+        },
+        Card {
+            number: Number::King,
+            shape: Shape::Hearts,
+        },
+        Card {
+            number: Number::Ace,
+            shape: Shape::Spades,
+        },
+        Card {
+            number: Number::Two,
+            shape: Shape::Spades,
+        },
+        Card {
+            number: Number::Three,
+            shape: Shape::Spades,
+        },
+        Card {
+            number: Number::Four,
+            shape: Shape::Spades,
+        },
+        Card {
+            number: Number::Five,
+            shape: Shape::Spades,
+        },
+        Card {
+            number: Number::Six,
+            shape: Shape::Spades,
+        },
+        Card {
+            number: Number::Seven,
+            shape: Shape::Spades,
+        },
+        Card {
+            number: Number::Eight,
+            shape: Shape::Spades,
+        },
+        Card {
+            number: Number::Nine,
+            shape: Shape::Spades,
+        },
+        Card {
+            number: Number::Ten,
+            shape: Shape::Spades,
+        },
+        Card {
+            number: Number::Jack,
+            shape: Shape::Spades,
+        },
+        Card {
+            number: Number::Queen,
+            shape: Shape::Spades,
+        },
+        Card {
+            number: Number::King,
+            shape: Shape::Spades,
+        },
+        Card {
+            number: Number::Ace,
+            shape: Shape::Diamonds,
+        },
+        Card {
+            number: Number::Two,
+            shape: Shape::Diamonds,
+        },
+        Card {
+            number: Number::Three,
+            shape: Shape::Diamonds,
+        },
+        Card {
+            number: Number::Four,
+            shape: Shape::Diamonds,
+        },
+        Card {
+            number: Number::Five,
+            shape: Shape::Diamonds,
+        },
+        Card {
+            number: Number::Six,
+            shape: Shape::Diamonds,
+        },
+        Card {
+            number: Number::Seven,
+            shape: Shape::Diamonds,
+        },
+        Card {
+            number: Number::Eight,
+            shape: Shape::Diamonds,
+        },
+        Card {
+            number: Number::Nine,
+            shape: Shape::Diamonds,
+        },
+        Card {
+            number: Number::Ten,
+            shape: Shape::Diamonds,
+        },
+        Card {
+            number: Number::Jack,
+            shape: Shape::Diamonds,
+        },
+        Card {
+            number: Number::Queen,
+            shape: Shape::Diamonds,
+        },
+        Card {
+            number: Number::King,
+            shape: Shape::Diamonds,
+        },
+    ];
 }
