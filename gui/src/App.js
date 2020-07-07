429842abdc5e381f31a9df7200998e57460cbc44
Assaf Morami <assaf.morami@gmail.com>
2020-07-07 20:25:01 +0300

    CANEM + secretjs now loads

--- gui/src/App.js
+++ gui/src/App.js
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Form, TextArea, Input, Icon } from "semantic-ui-react";
+import SecretJS from "secretjs";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "semantic-ui-css/semantic.min.css";
+import "./App.css";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return <div>banana</div>;
+  }
 }
 
 export default App;
